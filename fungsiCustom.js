6759a528c5e340c16148ca93a3e3e8d37f98f316
daffamukti <daffamuktizaelani97@gmail.com>
2022-06-12 21:06:29 +0700

    Challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,46 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let err = false;
+  let data = [];
+  
+  //Membaca file ke 1
+  fs.readFile(file1, {encoding : 'utf8'}, (error, datafile) => {
+    if(error){
+      err = console.log("Terjadi error pada data 1" + error);
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject.message.split(" ")[1])
+  });
+
+  //Membaca file ke 2
+  fs.readFile(file2, {encoding : 'utf8'}, (error, datafile) => {
+    if(error){
+      err = console.log("Terjadi error pada data 2" + error);
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject[0].message.split(" ")[1])
+
+  });
+
+  //Membaca file ke 3
+  fs.readFile(file3, {encoding : 'utf8'}, (error, datafile) => {
+    if(err){
+      err = console.log("Terjadi error pada data 3" + error);
+      fnCallback(err, data);
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject[0].data.message.split(" ")[1])
+    fnCallback(err, data);
+  });
+};
+
+
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
